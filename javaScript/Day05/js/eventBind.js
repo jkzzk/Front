ff2db349242a18f14824f40d5fbd89df3d660ce2
jkzzk <1745493589@qq.com>
2020-04-17 16:32:24 +0800

    js 拖拽

--- javaScript/Day05/js/eventBind.js
+++ javaScript/Day05/js/eventBind.js
@@ -9,8 +9,6 @@ function bind(obj,eventName,callback) {
 }
 
 function unbind(obj,eventName,callback) {
-    console.log(obj);
-    console.log(obj.removeEventListener);
     if(obj.removeEventListener) {
         obj.removeEventListener(eventName,callback,false);
     }else {
