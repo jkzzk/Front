7bbce541afb82e802d6328ba9aa3166db7b32803
jkzzk <1745493589@qq.com>
2020-04-17 10:04:38 +0800

    js 拖拽

--- javaScript/Day05/js/eventBind.js
+++ javaScript/Day05/js/eventBind.js
@@ -7,3 +7,15 @@ function bind(obj,eventName,callback) {
         });
     }
 }
+
+function unbind(obj,eventName,callback) {
+    console.log(obj);
+    console.log(obj.removeEventListener);
+    if(obj.removeEventListener) {
+        obj.removeEventListener(eventName,callback,false);
+    }else {
+        obj.detachEvent("on" + eventName,function () {
+            callback.call(obj);
+        });
+    }
+}
